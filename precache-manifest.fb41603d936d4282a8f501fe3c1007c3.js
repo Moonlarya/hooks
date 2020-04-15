953ded589bc061f24001ac7b24c67af079d86b1d
Moonlarya <gum225@gmail.com>
2020-04-15 17:46:01 +0300

    Updates

---
+++ precache-manifest.fb41603d936d4282a8f501fe3c1007c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b070acbc35d53251e7be3b0866122c7d",
+    "url": "/hooks/index.html"
+  },
+  {
+    "revision": "891e79fb44d9bd7376db",
+    "url": "/hooks/static/css/main.def96890.chunk.css"
+  },
+  {
+    "revision": "993624c4ff1837392d9a",
+    "url": "/hooks/static/js/2.4ddfdc05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooks/static/js/2.4ddfdc05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891e79fb44d9bd7376db",
+    "url": "/hooks/static/js/main.c803dc95.chunk.js"
+  },
+  {
+    "revision": "ecebd2135d1b9c2b0c8b",
+    "url": "/hooks/static/js/runtime-main.fc772784.js"
+  }
+]);
